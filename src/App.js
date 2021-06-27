3e8a00c406e8a79d30127b5d5d3b023e62a231e1
Eric Vuu <ericlv91@gmail.com>
2021-06-27 09:08:55 -0700

    completed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Suspense, useRef} from "react";
+import React, {Suspense, useRef, useEffect} from "react";
 import "./App.scss";
 
 //Components
@@ -10,31 +10,41 @@ import { Html} from "@react-three/drei";
 import { useLoader } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
-const Model = () => {
-  const gltf = useLoader(GLTFLoader, "./armchairYellow.gltf");
+// page state
+import state from "./components/state";
+
+// intersection observer
+import { useInView } from "react-intersection-observer";
+
+const Model = ({modelPath}) => {
+  const gltf = useLoader(GLTFLoader, modelPath);
   return <primitive object={gltf.scene} />;
 }
 
-const HTMLContent = () => {
-
+const HTMLContent = ({ bgColor, domContent, children, modelPath, positionY }) => {
   const ref = useRef();
-  useFrame(() => (ref.current.rotation.y += 0.01))
+  useFrame(() => (ref.current.rotation.y += 0.01));
+  const [refItem, inView] = useInView({
+    threshold: 0,
+  });
+
+  useEffect(() => {
+    inView && (document.body.style.background = bgColor);
+  }, [bgColor, inView]);
 
   return (
     <Section factor={1.5} offset={1}>
-      <group position={[0, 250, 0]}>
+      <group position={[0, positionY, 0]}>
         <mesh ref={ref} position={[0, -35, 0]}>
-          <Model/>
+          <Model modelPath={modelPath} />
         </mesh>
-        <Html fullscreen>
-          <div className="container">
-            <h1 className="title">Hello</h1>
-          </div>
+        <Html portal={domContent} fullscreen>
+          <div className="container" ref={refItem}>{children}</div>
         </Html>
       </group>
     </Section>
   );
-}
+};
 
 const Lights = () => {
   return (
@@ -48,15 +58,48 @@ const Lights = () => {
 }
 
 export default function App() {
+  const domContent = useRef();
+  const scrollArea = useRef();
+
+  const onScroll = (e) => (state.top.current = e.target.scrollTop)
+
+  useEffect(() => void onScroll({target: scrollArea.current}), [])
   return (
     <>
       <Header />
       <Canvas colorManagement camera={{ position: [0, 0, 120], fov: 70 }}>
-        <Lights/>
+        <Lights />
         <Suspense fallback={null}>
-          <HTMLContent />
+          <HTMLContent
+            bgColor="#f15946"
+            domContent={domContent}
+            modelPath="./armchairYellow.gltf"
+            positionY={250}
+          >
+            <h1 className="title">Yellow</h1>
+          </HTMLContent>
+          <HTMLContent
+            bgColor="#571ec1"
+            domContent={domContent}
+            modelPath="./armchairGreen.gltf"
+            positionY={0}
+          >
+            <h1 className="title">Green</h1>
+          </HTMLContent>
+          <HTMLContent
+            bgColor="#636567"
+            domContent={domContent}
+            modelPath="./armchairGray.gltf"
+            positionY={-250}
+          >
+            <h1 className="title">Gray</h1>
+          </HTMLContent>
         </Suspense>
       </Canvas>
+      <div className="scrollArea" ref={scrollArea} onScroll={onScroll}>
+        <div style={{ position: "sticky", top: 0 }} ref={domContent}></div>
+        <div style={{ height: `${state.sections * 100}vh` }}></div>
+      </div>
     </>
   );
 }
